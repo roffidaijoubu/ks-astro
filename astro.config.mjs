6bc66f6cf8044105955c3c8afc7971065b0d7bd0
Roffi Daijoubu <119045709+roffidaijoubu@users.noreply.github.com>
2023-12-15 04:46:58 +0700

    search kinda janky

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import orama from '@orama/plugin-astro';
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), orama({
+    // We can generate more than one DB, with different configurations
+    mydb: {
+      // Required. Only pages matching this path regex will be indexed
+      pathMatcher: /baca\/.+/,
+      // Optional. 'english' by default
+      // language: 'indonesian',
+
+      // Optional. ['body'] by default. Use it to constraint what is used to
+      // index a page.
+      contentSelectors: ['main']
+    }
+  }), react(), vue(), svelte()]
 });
---
+++ src/components/OramaSearch.svelte
@@ -0,0 +1,97 @@
+<script>
+  import {
+    getOramaDB,
+    search as searchOrama,
+  } from "@orama/plugin-astro/client";
+  import { writable } from "svelte/store";
+
+  const result = writable([]);
+  const showDiv = writable(false);
+
+  async function search(searchTerm) {
+    if (searchTerm.length > 2) {
+      const db = await getOramaDB("mydb");
+      const res = await searchOrama(db, { term: searchTerm });
+      let hits = res.hits.map((hit) => ({
+        title: hit.document.title,
+        path: hit.document.path,
+        content: processContent(searchTerm, hit.document.content),
+      }));
+
+      showDiv.set(hits.length > 0);
+      result.set(hits);
+    } else {
+      showDiv.set(false);
+    }
+  }
+
+  function cleanContent(content) {
+    return content.replace(/<|>|\*|\[.*?\]/g, "");
+  }
+
+  function processContent(title, content) {
+    content = cleanContent(content);
+
+    // Highlight the title in the content
+    const regex = new RegExp(`\\b${escapeRegExp(title)}\\b`, "gi");
+    content = content.replace(
+      regex,
+      `<span class="bg-accent text-accent-content">${title}</span>`,
+    );
+
+    // Find the highlighted title's index
+    const highlightStartIndex = content
+      .toLowerCase()
+      .indexOf(
+        `<span class="bg-accent text-accent-content">${title.toLowerCase()}</span>`,
+      );
+    if (highlightStartIndex === -1) {
+      return content.length > 200 ? content.substring(0, 200) + "..." : content;
+    }
+
+    // Extract snippet around the title
+    const snippetStart = Math.max(highlightStartIndex - 100, 0); // 100 characters before the title
+    const snippetEnd = Math.min(
+      highlightStartIndex + 100 + title.length,
+      content.length,
+    ); // 100 characters after the title
+
+    return (
+      content.substring(snippetStart, snippetEnd) +
+      (snippetEnd < content.length ? "..." : "")
+    );
+  }
+
+  // Function to escape RegExp special characters in the title
+  function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+</script>
+
+<div class="p-4">
+  <input
+    type="text"
+    class="searchInputBox h-15 p-2 border rounded"
+    on:input={(event) => search(event.target.value)}
+  />
+  {#if $showDiv}
+    <div id="searchResults" class="fixed border rounded p-5 z-10 bg-base-200">
+      <ul class="menu">
+        {#each $result as element, index}
+          <li>
+            <a href={element.path} class="block">
+              <div class="text-base-content">
+                <div class="font-bold">{element.title}</div>
+                <div class="text-sm">{@html element.content}</div>
+              </div>
+            </a>
+          </li>
+
+          {#if index !== $result.length - 1}
+            <br />
+          {/if}
+        {/each}
+      </ul>
+    </div>
+  {/if}
+</div>
