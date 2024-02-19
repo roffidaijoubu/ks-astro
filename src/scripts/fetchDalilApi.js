8a89f7c82b1995fd72a3a5160900902f6cc12dfb
Developer <roffidaijoubu@gmail.com>
2024-02-20 02:41:52 +0700

    Modal

---
+++ src/scripts/fetchDalilApi.js
@@ -0,0 +1,19 @@
+import fetch from 'node-fetch';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+async function fetchDalilApi() {
+  const response = await fetch('https://ks.valocg.xyz/wp-json/api/dalil');
+  const data = await response.json();
+  
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const filePath = path.join(dirname, '../../public/api/dalil.json');
+  
+  // Ensure the directory exists
+  await fs.mkdir(path.dirname(filePath), { recursive: true });
+  
+  await fs.writeFile(filePath, JSON.stringify(data));
+}
+
+fetchDalilApi().catch(console.error);
